db8c7f354e7a198425b46f2e497ce743e150d2da
Justin P <jadamsperry@gmail.com>
2020-04-27 15:13:27 -0600

    More HTML and JS

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Hello World, this is some JS!")
+
+function My_First_Function() {
+		var string = 'Kiss me, I'm Irish';
+		var result = string.fontcolor('green');
+		document.GetElementByID("Irish").innerHTML = result;
+		};
