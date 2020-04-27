a10fe56283fbfe97580c08169ef3abdb3d23cf1f
Justin P <jadamsperry@gmail.com>
2020-04-27 16:11:57 -0600

    javascript case fix
    fix: ID to Id in JS file

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -1,5 +1,5 @@
 function My_First_Function() {
 		var string = "Kiss me, I'm Irish";
 		var result = string.fontcolor('green');
-		document.getElementByID("Irish").innerHTML = result;
+		document.getElementById("Irish").innerHTML = result;
 		}
